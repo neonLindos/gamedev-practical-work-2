88c5670543497bb3ea6e1d336dd35fe6c667e1fd
neonLandos <neonlake443@gmail.com>
2025-10-06 12:27:10 +0500

    add more

---
+++ Assets/scripts/JumpCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class JumpCube : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float rayDistance = 1.1f; // длина луча вниз
+    public Rigidbody rb;
+
+  
+
+    private void Update()
+    {
+        // стреляем лучом вниз от центра куба
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, rayDistance))
+        {
+            if (hit.collider.CompareTag("floor"))
+            {
+                Debug.Log("Ray hit floor — jumping!");
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * rayDistance);
+    }
+}
